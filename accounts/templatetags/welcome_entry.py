2a09591749b3928971ef2cdd08c3e22614d9c0e8
Ryan Hodges <ryan.d.hodges@gmail.com>
2017-07-25 12:39:57 -0700

    removing default ugly welcome_entry content until we are better able to override it

--- accounts/templatetags/welcome_entry.py
+++ accounts/templatetags/welcome_entry.py
@@ -3,8 +3,10 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def welcome_entry(context):
-    return "Fake Welcome Entry!"
+    # return "Fake Welcome Entry!"
+    return ""
 
 @register.simple_tag(takes_context=True)
 def welcome_title(context):
-    return "Fake Welcome Title!"
+    # return "Fake Welcome Title!"
+    return ""
